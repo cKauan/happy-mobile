2e55e0051ef6e25374b121de683f588ff3f9be65
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-10-17 23:47:50 -0300

    Add project routes :hammer:

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,12 +6,43 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from "./pages/OrphanagesMap";
 import OrphanageDetails from "./pages/OrphanageDetails";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import Header from "./components/Header";
 const Routes = () => {
     return (
         <Router>
-            <Navigator screenOptions={{ headerShown: false }}>
+            <Navigator
+                screenOptions={{
+                    headerShown: false,
+                    cardStyle: { backgroundColor: "#f2f3f5" },
+                }}
+            >
                 <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+                <Screen
+                    name="OrphanageDetails"
+                    component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCloseButton={false} title="Orfanato"/>,
+                    }}
+                />
+                <Screen
+                    name="OrphanageData"
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados" />,
+                    }}
+                />
+                <Screen
+                    name="SelectMapPosition"
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no mapa"/>,
+                    }}
+                />
             </Navigator>
         </Router>
     );
